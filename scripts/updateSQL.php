b410af6a665535bd48b8b5570a4ee9ed5e83847f
Leindfraust <lionheart023@yahoo.com>
2019-07-23 20:51:58 +0800

    update and working version of update sql

--- scripts/updateSQL.php
+++ scripts/updateSQL.php
@@ -1,9 +1,13 @@
 <?php 
 include "config.php";
 
-if(isset($_POST['ready'])){
+    $index = "ID=".$_GET['IDNo']; 
 
-$sql = "UPDATE orderlist SET ID=00001 WHERE Status='ready for pick up'";
+    $sql = "UPDATE orderlist SET Status='pending' WHERE ".$index;
 
-}
+    if (mysqli_query($con, $sql)) {
+    echo 'Record updated successfully click <a href="../data-display.html">here</a> to return';
+    } else {
+    echo "Error updating record: " . mysqli_error($con);
+    }
 ?>
