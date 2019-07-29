7b3c20a998baaec3b310ef38538ea4868d23553f
Leindfraust <lionheart023@yahoo.com>
2019-07-30 02:32:34 +0800

    Update journal-page.php

---
+++ journal-page.php
@@ -0,0 +1,22 @@
+<?php session_start();
+ if(!isset($_SESSION['jorunalist'])){
+        header("Location:journalist-login.php");
+     }
+          echo "Login Success";
+?>
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="css/img/lcclogo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bulma-0.7.1/css/bulma.css">
+    <link rel="stylesheet" href="css/bulma-0.7.1/css/bulma.css.map">
+    <title>Journalist</title>
+</head>
+
+<body>
+    
+</body>
+</html>
